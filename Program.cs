01a9405baa6ff317f4275676dc665f8b00971d37
Vakil <burangulov.vakil@gmail.com>
2025-05-03 21:21:52 +0300

    Задание 13.6.2 (HW-03)

--- Program.cs
+++ Program.cs
@@ -1,86 +1,51 @@
-﻿using System.Diagnostics;
-
-namespace Net
+﻿namespace Net
 {
     class Program
     {
         static void Main(string[] args)
         {
             var text = File.ReadAllText("D:\\Downloads\\input.txt");
-            var textForChars =  text.ToCharArray();
+            var noPunctuationText = new string(text.Where(c => !char.IsPunctuation(c)).ToArray());
+
+            var splitChars = new[] { " ", "\n", "\r" };
+
+            var words = noPunctuationText.Split(splitChars, StringSplitOptions.RemoveEmptyEntries);
             
-            var listAddToFirst =  new List<char>(textForChars);
-            var linkedListAddToFirst = new LinkedList<char>(textForChars);
-            
-            var listAddToMiddle =  new List<char>(textForChars);
-            var linkedListAddToMiddle = new LinkedList<char>(textForChars);
-            
-            var listAddToLast =  new List<char>(textForChars);
-            var linkedListAddToLast = new LinkedList<char>(textForChars);
-            
-            
-            
-            
-            // Add to first
-            var timer = Stopwatch.StartNew();
-            AddValuesToMiddleList(listAddToFirst, 'h', 0);
-            var timeList = timer.Elapsed.TotalMilliseconds;
-            
-            timer = Stopwatch.StartNew();
-            linkedListAddToFirst.AddFirst('h');
-            var timeLinkedList = timer.Elapsed.TotalMilliseconds;
-            
-            Console.WriteLine($"Время добавдение элемента в начало листа\n" +
-                              $"Цепь: {timeLinkedList} мс\n" +
-                              $"Простой лист: {timeList} мс");
-            Console.WriteLine();
-            
-            
-            
-            // Add to middle
-            var middleIndex = Convert.ToInt32(textForChars.Length / 2);
-            timer = Stopwatch.StartNew();
-            AddValuesToMiddleList(listAddToMiddle, 'h', middleIndex);
-            timeList = timer.Elapsed.TotalMilliseconds;
-            
-            timer = Stopwatch.StartNew();
-            var middleValue = linkedListAddToMiddle.Find(textForChars[middleIndex]);
-            if (middleValue != null)
-                linkedListAddToMiddle.AddAfter(middleValue, 'h');
-            timeLinkedList = timer.Elapsed.TotalMilliseconds;
-            
-            Console.WriteLine($"Время добавдение элемента в середину листа\n" +
-                              $"Цепь: {timeLinkedList} мс\n" +
-                              $"Простой лист: {timeList} мс");
-            Console.WriteLine();
-            
-            
-            
-            // Add to last
-            timer = Stopwatch.StartNew();
-            listAddToLast.Add('h');
-            timeList = timer.Elapsed.TotalMilliseconds;
-            
-            timer = Stopwatch.StartNew();
-            linkedListAddToLast.AddLast('h');
-            timeLinkedList = timer.Elapsed.TotalMilliseconds;
-            
-            Console.WriteLine($"Время добавдение элемента в конец листа\n" +
-                              $"Цепь: {timeLinkedList} мс\n" +
-                              $"Простой лист: {timeList} мс");
-            Console.WriteLine();
+            var dict = new Dictionary<string, int>();
+
+            foreach (var word in words)
+            {
+                if (dict.ContainsKey(word.ToUpper()))
+                    dict[word.ToUpper()]++;
+                
+                else
+                    dict.Add(word.ToUpper(), 1);
+            }
+
+            var top10Words = GetTop10Words(dict);
+            var count = 1;
+
+            foreach (var i in top10Words)
+            {
+                Console.WriteLine($"{count}ое место занимает слово '{i.ToLower()}' с {dict[i]} повторениями");
+                count++;
+            }
         }
 
-        public static void AddValuesToMiddleList(List<char> list, char addingValue, int index)
+        static string[] GetTop10Words(Dictionary<string, int> dict)
         {
-            for (int i = list.Count; i > index; i--)
+            var top10Words = new string[10];
+
+            var dictClone = new Dictionary<string, int>(dict);
+
+            for (int i = 0; i < top10Words.Length; i++)
             {
-                if (i == list.Count)
-                    list.Add('a');
-                list[i] = list[i-1];
+                var max = dictClone.MaxBy(kvp => kvp.Value).Key;
+                top10Words[i] = max;
+                dictClone.Remove(max);
             }
             
-            list[index] = addingValue;
+            return top10Words;
         }
     }
 }
